5459cf89c8dd23b5acdc85de6fd24c3ca7be43c5
Justin Griffin <griffin.justintime@gmail.com>
2021-03-30 20:27:15 -0400

    fetching api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
 console.log("hello world");
+
+function getWeather(){
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+    
+    fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+    });
+
+}
+
+getWeather();
